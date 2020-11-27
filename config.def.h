b6307ea6727da2f5049d690667bb04dd5671b652
Voyen <dev@voyen.io>
2020-11-27 16:33:08 +1030

    barpadding

--- config.def.h
+++ config.def.h
@@ -5,6 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int sidepad	    = 18;	/* horizontal padding for bar */
+static const int vertpad	    = 18;	/* vertical padding for bar */
 static const int horizpadbar        = 12;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 12;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10" };
