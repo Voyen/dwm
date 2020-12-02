ab8d9da0ab4639e97f37d1d040df292b9679837b
Voyen <dev@voyen.io>
2020-12-02 13:16:12 +1030

    added ru_gaps

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static unsigned int borderpx  		= 2;        	/* border pixel of windows */
+static int gappx			= 18;		/* gaps between windows */
 static unsigned int snap      		= 32;       	/* snap pixel */
 static const int swallowfloating    	= 0;        	/* 1 means swallow floating windows by default */
 static int showbar            		= 1;        	/* 0 means no bar */
@@ -120,6 +121,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,			XK_minus,  setgaps,	   {.i = -10 } },
+	{ MODKEY,			XK_equal,  setgaps,	   {.i = +10 } },
+	{ MODKEY|ShiftMask,		XK_equal,  setgaps,	   {.i = 0 } },
     	{ MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
